eb1204c7c6a1ef3d3a36a59327988a1b74532774
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-02 14:11:33 +0800

    feat: add day2 solution

--- Code/02_aoc.py
+++ Code/02_aoc.py
@@ -1,13 +1,27 @@
-import os
-import math
-import re
+def part1(input):
+    rules = {
+        "A X": 4, "A Y": 8, "A Z": 3,
+        "B X": 1, "B Y": 5, "B Z": 9,
+        "C X": 7, "C Y": 2, "C Z": 6
+    }
+    score = 0
+    for i in input:
+        score += rules[i]
 
-os.system("cls")
-print("\n\nNEW INPUT\n")
+    return score
 
-def arrayToInt(array): return [int(i) for i in array]
+def part2(input):
+    rules = {
+        "A X": 3, "A Y": 4, "A Z": 8,
+        "B X": 1, "B Y": 5, "B Z": 9,
+        "C X": 2, "C Y": 6, "C Z": 7
+    }
 
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+    score = 0
+    for i in input:
+        score += rules[i]
+
+    return score
 
 fp = open("../Input/02_input.txt", "r").read().split("\n")
 
@@ -15,4 +29,6 @@ inputNum = []
 for i in fp: 
     inputNum.append(i)
 
-print(inputNum)
+
+print(part1(inputNum))
+print(part2(inputNum))
