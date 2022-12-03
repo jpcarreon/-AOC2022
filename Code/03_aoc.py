2a7bbabb4ef2d9d8c99bddd5b20404ea40cb9226
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-03 13:25:57 +0800

    feat: add day3 answers

--- Code/03_aoc.py
+++ Code/03_aoc.py
@@ -1,13 +1,33 @@
-import os
-import math
-import re
+def getPriority(type):
+    if (type.islower()):
+        return ord(type) - 96
+    else:
+        return ord(type) - 38
 
-os.system("cls")
-print("\n\nNEW INPUT\n")
+def part1(input):
+    sum = 0
 
-def arrayToInt(array): return [int(i) for i in array]
+    for i in input:
+        bagA = set(i[:len(i) // 2])
+        bagB = set(i[len(i) // 2:])
 
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+        dupe = (bagA & bagB).pop()
+        sum += getPriority(dupe) 
+
+    return sum
+
+def part2(input):
+    sum = 0
+
+    for i in range(0, len(input) - 2, 3):
+        bagA = set(input[i])
+        bagB = set(input[i + 1])
+        bagC = set(input[i + 2])
+
+        dupe = (bagA & bagB & bagC).pop()
+        sum += getPriority(dupe)
+
+    return sum
 
 fp = open("../Input/03_input.txt", "r").read().split("\n")
 
@@ -15,4 +35,5 @@ inputNum = []
 for i in fp: 
     inputNum.append(i)
 
-print(inputNum)
+print(part1(inputNum))
+print(part2(inputNum))
