4a20e7907a00e514128b5a01f8c550479fa6b30c
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-04 13:16:39 +0800

    feat: add day4 solutions

--- Code/04_aoc.py
+++ Code/04_aoc.py
@@ -1,18 +1,36 @@
-import os
-import math
-import re
-
-os.system("cls")
-print("\n\nNEW INPUT\n")
-
 def arrayToInt(array): return [int(i) for i in array]
 
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+def part1(input):
+    count = 0
+    for i in input:
+        pair1 = set(range(i[0][0], i[0][1] + 1))
+        pair2 = set(range(i[1][0], i[1][1] + 1))
 
-fp = open("../Input/04_input2.txt", "r").read().split("\n")
+        if (pair1.issubset(pair2) or pair2.issubset(pair1)):
+            count += 1
+
+    return count
+
+def part2(input):
+    count = 0
+    for i in input:
+        pair1 = set(range(i[0][0], i[0][1] + 1))
+        pair2 = set(range(i[1][0], i[1][1] + 1))        
+
+        if (pair1 & pair2):
+            count += 1
+    
+    return count
+
+fp = open("../Input/04_input.txt", "r").read().split("\n")
 
 inputNum = []
 for i in fp: 
-    inputNum.append(i)
+    temp = i.split(",")
+    temp[0] = arrayToInt(temp[0].split("-"))
+    temp[1] = arrayToInt(temp[1].split("-"))
 
-print(inputNum)
+    inputNum.append(temp)
+
+print(part1(inputNum))
+print(part2(inputNum))
