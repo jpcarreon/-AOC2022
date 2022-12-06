70e19cfde9301e94e8de4cefb50d5e9dfc267eb5
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-06 13:14:23 +0800

    feat: add day6 solutions

--- Code/06_aoc.py
+++ Code/06_aoc.py
@@ -1,35 +1,9 @@
-import os
-import math
-import re
+def findMarker(input, rollover):
+    for i in range(0, len(input) - rollover):
+        if len(set(input[i:i + rollover])) == rollover:
+            return i + rollover
 
-os.system("cls")
-print("\n\nNEW INPUT\n")
+fp = open("../Input/06_input.txt", "r").read().strip()
 
-def arrayToInt(array): 
-    newArray = []
-    for i in array:
-        try:
-            newArray.append(int(i))
-        except:
-            newArray.append(i)
-    return newArray
-
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
-
-def dictDeepCopy(dictionary): return {i: j.copy() for i,j in dictionary.items()}
-
-def part1(input):
-    return 0
-
-def part2(input):
-    return 0
-
-fp = open("../Input/06_input2.txt", "r").read().split("\n")
-
-inputNum = []
-for i in fp: 
-    inputNum.append(i)
-
-print(inputNum)
-print(part1(inputNum))
-print(part2(inputNum))
+print(findMarker(fp, 4))
+print(findMarker(fp, 14))
