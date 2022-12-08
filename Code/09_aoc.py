e05c4f6ddf10b983c92c1d41a3e92e5489386688
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-08 14:47:23 +0800

    feat: add day9 template

---
+++ Code/09_aoc.py
@@ -0,0 +1,35 @@
+import os
+import math
+import re
+
+os.system("cls")
+print("\n\nNEW INPUT\n")
+
+def arrayToInt(array): 
+    newArray = []
+    for i in array:
+        try:
+            newArray.append(int(i))
+        except:
+            newArray.append(i)
+    return newArray
+
+def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+
+def dictDeepCopy(dictionary): return {i: j.copy() for i,j in dictionary.items()}
+
+def part1(input):
+    return 0
+
+def part2(input):
+    return 0
+
+fp = open("../Input/09_input2.txt", "r").read().split("\n")
+
+inputNum = []
+for i in fp: 
+    inputNum.append(i)
+
+print(inputNum)
+print(part1(inputNum))
+print(part2(inputNum))
