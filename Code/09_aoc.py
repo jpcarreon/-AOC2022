82441c74fcb1a1e29deca3e0f89d20022adaa47b
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-09 15:43:54 +0800

    feat: add day9 solutions

--- Code/09_aoc.py
+++ Code/09_aoc.py
@@ -1,35 +1,43 @@
-import os
-import math
-import re
+def ropeMovement(input, length):
+    ropePosition = {i: [0, 0] for i in range(0, length + 1)}
+    visited = {(0, 0)}
+    dirs = {"R": [1, 0], "L": [-1, 0], "U": [0, 1], "D": [0, -1]}
 
-os.system("cls")
-print("\n\nNEW INPUT\n")
+    for i in input:
+        i = i.split()
+        
+        for _ in range(int(i[1])):
+            ropePosition[0][0] += dirs[i[0]][0]
+            ropePosition[0][1] += dirs[i[0]][1]
 
-def arrayToInt(array): 
-    newArray = []
-    for i in array:
-        try:
-            newArray.append(int(i))
-        except:
-            newArray.append(i)
-    return newArray
+            for rope, curr in ropePosition.items():
+                if (rope == 0): continue
+                prev = ropePosition[rope - 1]
 
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+                if (abs(prev[0] - curr[0]) > 1 or abs(prev[1] - curr[1]) > 1):
+                    if (abs(prev[0] - curr[0]) >= 1 and abs(prev[1] - curr[1]) >= 1):
+                        curr[0] += 1 if (prev[0] - curr[0] > 0) else -1
+                        curr[1] += 1 if (prev[1] - curr[1] > 0) else -1
 
-def dictDeepCopy(dictionary): return {i: j.copy() for i,j in dictionary.items()}
+                        if (rope == length): visited.add((curr[0], curr[1]))
 
-def part1(input):
-    return 0
+                    elif (abs(prev[0] - curr[0]) > 1):
+                        curr[0] += 1 if (prev[0] - curr[0] > 0) else -1
 
-def part2(input):
-    return 0
+                        if (rope == length): visited.add((curr[0], curr[1]))
+                    
+                    elif (abs(prev[1] - curr[1]) > 1):
+                        curr[1] += 1 if (prev[1] - curr[1] > 0) else -1
 
-fp = open("../Input/09_input2.txt", "r").read().split("\n")
+                        if (rope == length): visited.add((curr[0], curr[1]))
+
+    return len(visited)
+
+fp = open("../Input/09_input.txt", "r").read().split("\n")
 
 inputNum = []
 for i in fp: 
     inputNum.append(i)
 
-print(inputNum)
-print(part1(inputNum))
-print(part2(inputNum))
+print(ropeMovement(inputNum, 1))
+print(ropeMovement(inputNum, 9))
