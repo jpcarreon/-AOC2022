b9d7f8aba70d5b92986a34caf0786d2632c6f216
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-12 14:22:32 +0800

    feat: add day12 solutions

--- Code/12_aoc.py
+++ Code/12_aoc.py
@@ -1,34 +1,101 @@
-import os
-import math
-import re
+import heapq as hq
 
-os.system("cls")
-print("\n\nNEW INPUT\n")
+def convertMaze(input):
+    maze = []
+    start = [0, -1]
+    end = [0, -1]
+    startLoc = []
 
-def arrayToInt(array): 
-    newArray = []
-    for i in array:
-        try:
-            newArray.append(int(i))
-        except:
-            newArray.append(i)
-    return newArray
+    for i in range(len(input)): 
+        if ("S" in input[i]):
+            start[0] = i
+            start[1] = input[i].rfind("S")
 
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+        if ("E" in input[i]):
+            end[0] = i
+            end[1] = input[i].rfind("E")
 
-def dictDeepCopy(dictionary): return {i: j.copy() for i,j in dictionary.items()}
+        newArray = []
+        for j in range(len(input[i])):
+            newArray.append(ord(input[i][j].lower()) - 96)
+            if (input[i][j] == "a"):
+                startLoc.append([i, j])
 
-def part1(input):
-    pass
+        maze.append(newArray)
 
-def part2(input):
-    pass
+    return [maze, start, end, startLoc]
 
-fp = open("../Input/12_input2.txt", "r").read().split("\n")
+def checkPath(maze, src, dest, goal):
+    src = maze[src[0]][src[1]]
+    dest = maze[dest[0]][dest[1]]
 
-inputNum = []
-for i in fp: 
-    inputNum.append(i)
+    if src + 1 >= dest:
+        return True
+    else:
+        return False
 
-print(part1(inputNum))
-#print(part2(inputNum))
+def checkSides(maze, c, goal):
+    neighbors = []
+
+    if c[0] + 1 < len(maze) and checkPath(maze, c, [c[0] + 1, c[1]], goal):
+        neighbors.append([c[0] + 1,c[1]])
+
+    if c[0] - 1 != -1 and checkPath(maze, c, [c[0] - 1, c[1]], goal):
+        neighbors.append([c[0] - 1,c[1]])
+
+    if c[1] + 1 < len(maze[0]) and checkPath(maze, c, [c[0], c[1] + 1], goal):
+        neighbors.append([c[0],c[1] + 1])
+    
+    if c[1] - 1 != -1 and checkPath(maze, c, [c[0], c[1] - 1], goal):
+        neighbors.append([c[0],c[1] - 1])
+
+    return neighbors
+
+def dijkstra(input, start, end):
+    pathRec = {}
+    distRec = {}
+    distRec[(start[0], start[1])] = 0
+    pathRec[(start[0], start[1])] = 0
+    pq = []
+    hq.heappush(pq, [0, start])
+
+    while (len(pq) != 0):
+        index = hq.heappop(pq)[1]
+
+        for i in checkSides(input, index, end):
+            if (i[0], i[1]) in distRec: continue
+
+            newDist = distRec[(index[0], index[1])] + input[i[0]][i[1]]
+
+            if (i[0], i[1]) not in distRec or newDist < distRec[(i[0], i[1])]:
+                distRec[(i[0], i[1])] = newDist
+                pathRec[(i[0], i[1])] = (index[0], index[1])
+                hq.heappush(pq, [newDist, i])
+    return pathRec
+
+def part1(input, start, end):
+    pathRec = dijkstra(input, start,end)
+    record = -1
+    current = (end[0], end[1])
+
+    if (current not in pathRec): return 999999
+    while (current != 0):
+        current = pathRec[current]
+        record += 1
+
+    return record
+
+def part2(input, aLoc, end):
+    minRecord = 999999
+    for i in aLoc:
+        rec = part1(input, i, (end[0], end[1]))
+        minRecord = min(minRecord, rec)
+
+    return minRecord
+
+input = open("../Input/12_input.txt", "r").read().split("\n")
+
+maze, start, end, startLoc = convertMaze(input)
+
+print(part1(maze, start, end))
+print(part2(maze, startLoc, end))
