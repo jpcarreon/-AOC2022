f703866ab855489e33e5c34c6b53a5582f9a0aa2
John Philip Carreon <jccarreon3@up.edu.ph>
2022-12-13 16:10:18 +0800

    feat: add day13 solutions

--- Code/13_aoc.py
+++ Code/13_aoc.py
@@ -1,34 +1,80 @@
 import os
-import math
-import re
-
 os.system("cls")
 print("\n\nNEW INPUT\n")
 
-def arrayToInt(array): 
-    newArray = []
-    for i in array:
-        try:
-            newArray.append(int(i))
-        except:
-            newArray.append(i)
-    return newArray
+def isInOrder(left, right):
+    maxLen = max(len(left), len(right))
 
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+    for i in range(maxLen):
+        if (i == len(left) and i < len(right)):
+            return True
+        elif (i == len(right) and i < len(left)):
+            return False
 
-def dictDeepCopy(dictionary): return {i: j.copy() for i,j in dictionary.items()}
+        lval = left[i]
+        rval = right[i]
+
+        if (isinstance(lval, int) and isinstance(rval, int)):
+            if lval < rval:
+                return True
+            elif lval > rval:
+                return False
+
+        elif (isinstance(lval, list) and isinstance(rval, list)):
+            ordered = isInOrder(lval, rval)
+            if ordered is not None: return ordered
+
+        elif (isinstance(lval, list)):
+            ordered = isInOrder(lval, [rval])
+            if ordered is not None: return ordered
+
+        elif (isinstance(rval, list)):
+            ordered = isInOrder([lval], rval)
+            if ordered is not None: return ordered
+        
+    return None
 
 def part1(input):
-    pass
+    sumParts = 0
+    for i in range(len(input)):
+        left, right = input[i]
+
+        if isInOrder(left, right):
+            sumParts += i + 1
+
+    return sumParts
+
 
 def part2(input):
-    pass
+    packets = [[2], [6]]
+    temp = None
+    answer = 1
 
-fp = open("../Input/10_input.txt", "r").read().split("\n")
+    for i in input:
+        packets.append(i[0])
+        packets.append(i[1])
+
+    for i in range(len(packets)):
+        for j in range(len(packets) - i - 1):
+            if not isInOrder(packets[j], packets[j + 1]):
+                temp = packets[j]
+                packets[j] = packets[j + 1]
+                packets[j + 1] = temp
+        
+    for i in range(len(packets)):
+        if packets[i] == [2]:
+            answer *= i + 1
+        elif packets[i] == [6]:
+            answer *= i + 1
+    
+    return answer
+
+fp = open("../Input/13_input.txt", "r").read().split("\n\n")
 
 inputNum = []
 for i in fp: 
-    inputNum.append(i)
+    np = [eval(j) for j in i.split("\n")]
+    inputNum.append(np)
 
 print(part1(inputNum))
-#print(part2(inputNum))
+print(part2(inputNum))
